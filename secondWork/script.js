87a9435cd876c8748a8ef91575809db33e5d157b
DimaKreed <dimonvarch2001@gmail.com>
2020-09-11 15:35:32 +0300

    Made changes

--- secondWork/script.js
+++ secondWork/script.js
@@ -58,7 +58,7 @@ let fourth=document.getElementById('4');
 }
 refreshPage10Times();
 
-z
+
 
 let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let [changedString,,arr_en_random]=cryptString(arr_en,'hi i am Dima');
